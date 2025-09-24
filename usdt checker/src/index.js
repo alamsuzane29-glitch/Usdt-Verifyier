98dca0693261c522db65373fbd417fe150b36031
alamsuzane29-glitch <alamsuzane29@gmail.com>
2025-09-24 19:24:36 +0530

    index.js

--- usdt checker/src/index.js
+++ usdt checker/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import App from "./App";
 
-// Create root and render App
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
